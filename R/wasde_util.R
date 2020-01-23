16ede7ec1546bd1660425f7ebdae304a949fde15
Naveen Mathew <naveenmathewnathan.s@gmail.com>
2020-01-23 18:22:34 +0530

    Moved functions around

--- R/wasde_util.R
+++ R/wasde_util.R
@@ -32,3 +32,37 @@ read_wasde <- function(path, sheet_name = "Page 15", col_range = "A13:A58",
   setwd(wd)
   return(com_data)
 }
+
+
+#' @title Clean the read-in WASDE soybean data
+#' @description Remove hiphens and asterisk as well as transpose the data into time-series data
+#'
+#' @param combined_data Data frame of the combined soybean data
+#' @return Data frame of the clean combined soybean data
+#' @examples
+#' data <- clean_wasde(combined_data = soybeanWASDE)
+#' @export
+
+# Example should be changed as it is for read_wasde
+clean_wasde=function(combined_data){
+  #remove hyphen
+  test=grepl("-",as.matrix(combined_data[,-1]))
+  test=matrix(test,dim(combined_data))
+  temp  <- strsplit(as.character(combined_data[,-1][test]), "-")
+  non_hyphen=sapply(temp, function(x) mean(as.numeric(x)))
+  combined_data[,-1][test]=non_hyphen
+
+  #remove asterisk
+  combined_data[,-1]=apply(combined_data[,-1],2,function(x){gsub("\\*",'',x)})
+
+  #as.numeric
+  combined_data[,-1]=apply(combined_data[,-1],2,as.numeric)
+
+  #transpose the data
+  trans_data=as.data.frame(t(combined_data[,-1]))
+  colnames(trans_data)=combined_data[,1]
+  trans_data=cbind(rownames(trans_data),trans_data)
+  colnames(trans_data)[1]='Date'
+  trans_data$Date=as.Date(trans_data$Date, format = "%m-%d-%y")
+  return(trans_data)
+}
